1e21506fe68ae1544cb282ebb281d6068ea4d573
KhaosVoid <ztommey@gmail.com>
2024-11-24 00:18:06 -0600

    Added assignment constructors for filters (#7)

--- src/Rested.Core.Data/Search/DateTimeFieldFilter.cs
+++ src/Rested.Core.Data/Search/DateTimeFieldFilter.cs
@@ -4,9 +4,27 @@ namespace Rested.Core.Data.Search;
 
 public class DateTimeFieldFilter : FieldFilter<DateTime?, DateTimeFieldFilterOperations>
 {
+    #region Properties
+    
     [JsonPropertyOrder(0)]
     public override FilterTypes FilterType { get; set; } = FilterTypes.DateTimeFieldFilter;
     
     [JsonPropertyOrder(2)]
     public override DateTimeFieldFilterOperations FilterOperation { get; set; }
+    
+    #endregion Properties
+    
+    #region Ctor
+
+    public DateTimeFieldFilter()
+    {
+        
+    }
+
+    public DateTimeFieldFilter(DateTimeFieldFilter dateTimeFieldFilter) : base(dateTimeFieldFilter)
+    {
+        
+    }
+    
+    #endregion Ctor
 }
--- src/Rested.Core.Data/Search/NumberFieldFilter.cs
+++ src/Rested.Core.Data/Search/NumberFieldFilter.cs
@@ -4,9 +4,27 @@ namespace Rested.Core.Data.Search;
 
 public class NumberFieldFilter : FieldFilter<int?, NumberFieldFilterOperations>
 {
+    #region Properties
+    
     [JsonPropertyOrder(0)]
     public override FilterTypes FilterType { get; set; } = FilterTypes.NumberFieldFilter;
     
     [JsonPropertyOrder(2)]
     public override NumberFieldFilterOperations FilterOperation { get; set; }
+    
+    #endregion Properties
+    
+    #region Ctor
+
+    public NumberFieldFilter()
+    {
+        
+    }
+
+    public NumberFieldFilter(NumberFieldFilter numberFieldFilter) : base(numberFieldFilter)
+    {
+        
+    }
+    
+    #endregion Ctor
 }
